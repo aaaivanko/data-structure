1fd0433e039b917b182e2b1c9c83ef1449650004
ivan <ivan.chvartatskyy@gmail.com>
2021-02-23 09:57:09 +0200

    adding count program

--- ex_object.py
+++ ex_object.py
@@ -1,13 +1,13 @@
-class Parrot():
-    def __init__(self, name, age, weight):
-        self.name = name
-        self.age = age
-        self.weight = weight
+# 5. Write a Python program to count the number of strings where the
+# string length is 2 or more and the first and last character are same from a given list of strings. Go to the editor
+# Sample List : ['abc', 'xyz', 'aba', '1221']
+# Expected Result : 2
 
-    def description(self):
-        return f"Hello, my name is {self.name}, I'm {self.age} years old"
+def count_number(my_list):
+    total = 0
+    for item in my_list:
+        if item[0] == item[-1] and len(item):
+            total += 1
+    return total
 
-parrot1 = Parrot('Ivan', 22, 80)
-
-
-print(parrot1.name)
+print(count_number(['abc', 'xyz', 'aba', '1221']))
